62edd19fc194a221433baa8f2480cd477a3955a7
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-04-21 00:54:53 +0200

    Add appointment scheduling functionality with user input

--- src/Client.java
+++ src/Client.java
@@ -1,27 +1,50 @@
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
     public static final int MAKE_APPOINTMENT_PORT = 6666;
     public static final int CANCEL_APPOINTMENT_PORT = 6667;
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.print("Please enter your name: ");
         Scanner scanner = new Scanner(System.in);
         String patientName = scanner.nextLine();
+        scanner.close();
         Socket makeSocket = null, cancelSocket = null;
+        PrintWriter out1 = null, out2 = null;
         try {
             // Open two sockets for make and cancel
             makeSocket = new Socket("localhost", MAKE_APPOINTMENT_PORT);
             cancelSocket = new Socket("localhost", CANCEL_APPOINTMENT_PORT);
             // Send patient's name to both sockets
-            PrintWriter out = new PrintWriter(makeSocket.getOutputStream());
-            out.println(patientName);
-            out = new PrintWriter(cancelSocket.getOutputStream());
-            out.println(patientName);
+            out1 = new PrintWriter(makeSocket.getOutputStream());
+            out1.println(patientName);
+            out2 = new PrintWriter(cancelSocket.getOutputStream());
+            out2.println(patientName);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader socketReader_make = new BufferedReader(new InputStreamReader(makeSocket.getInputStream()));
+        BufferedReader socketReader_cancel = new BufferedReader(new InputStreamReader(cancelSocket.getInputStream()));
+        while (true) {
+            System.out.println("Enter\n 1 to make a new appointment\n 2 to cancel an appointment");
+            int inpt = scanner.nextInt();
+            System.out.print("Enter Doctor ID: \n");
+            String docID = consoleReader.readLine();
+            System.out.println("Enter Timeslot: ");
+            int timeSlot = Integer.parseInt(consoleReader.readLine());
+            String response = null;
+            if(inpt == 1) {
+                // make appointment
+                out1.println(docID + ' ' + timeSlot);
+                response = socketReader_make.readLine();
+            } else {
+                // cancel appointment
+                out2.println(docID + ' ' + timeSlot);
+                response = socketReader_cancel.readLine();
+            }
+            System.out.println("Result: " + response);
         }
     }
 }
